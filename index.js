b56e24e6e51f779d3d7b55a721ec1759ed008ae5
Orlando Nelson <nelson8223@gmail.com>
2023-01-17 16:43:12 -0500

    added a Readme

---
+++ index.js
@@ -0,0 +1,72 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = ["Do you have a Readme File?"];
+const fs = require("fs")
+const inquirer = require('inquirer');
+console.log("hello")
+const generateMarkdown = require('./utils/generateMarkdown')
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your Project Title?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'what is Your Project Description?',
+      name: 'Description',
+    },
+    {
+      type: 'input',
+      message: 'what are the credits used?',
+      name: 'Credits',
+    },
+    {
+        type: 'input',
+        message: 'What is the instructions for the Usage?',
+        name: 'Usage',
+},
+{
+    type: 'input',
+    message: 'what are the steps for the Installation?',
+    name: 'Installation',
+},
+
+{
+    type: 'input',
+    message: 'what is the License used ?',
+    name: 'License',
+},
+
+
+  ])
+  .then((response) => {
+  // console.log(response)
+  init(response)
+ 
+  });
+
+
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init(data) {
+  
+  const createReadMe = generateMarkdown(data)
+  console.log(createReadMe)
+fs.writeFile("./utils/README.md", createReadMe, (err)=>{ 
+ if (err) throw new Error('something went wrong')
+} )
+
+}
+
+// Function call to initialize app
+// init();
---
+++ utils/README.md
@@ -0,0 +1,24 @@
+# undefined
+
+  ## Table of Content
+  -[Description](#Description)
+  -[Usage](#Usage)
+  -[Installation](#Installation)
+  -[Credits](#credits)
+  -[License](#license)
+
+  ## Description
+  about to add a Read me
+
+  ## Usage
+  added the added
+
+  ## Installation
+  added to thed list
+
+  ## Credits
+  node js
+
+  ## License
+  node ls
+
